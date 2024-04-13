dc3c082c69c388ad212e42fd849424154e9f5946
Muhammad Owais <owaisilyas22@gmail.com>
2024-04-13 19:10:27 +0500

    Minor changes on design with font change and button layout change. clicking effect of button are added. Copy button is fix and working correctly using useRef

--- src/App.tsx
+++ src/App.tsx
@@ -10,8 +10,8 @@ const [allowChar, setAllowChar] = useState(false);
 const passRef = useRef<HTMLInputElement | null>(null);
 
 const copyToClip = useCallback(() => {
-  // passRef.current?.select();
-  // passRef.current?.setSelectionRange(0, 999);
+  passRef.current?.select();
+  passRef.current?.setSelectionRange(0, 999);
   window.navigator.clipboard.writeText(password)
 }, [password])
 
@@ -41,28 +41,33 @@ useEffect(()=>{
     <div className="bg-cover bg-no-repeat bg-center w-full h-screen " style={{backgroundImage: "url('./public/Image/bg-password.jpg')"}}>
 
      <div className='max-w-screen-sm mx-auto pt-20'  >
-      <h1 className='text-center text-amber-600 font-extrabold drop-shadow-lg text-4xl '>Welcome to Your Password Generator</h1>
-      <div className=' flex justify-center items-center bg-amber-400 hover:bg-amber-500 drop-shadow-xl box-shadow-lg border-amber-900 border-2   rounded-lg py-4 px-10'>
+      <h1 className='text-center font-outfit text-amber-600 font-extrabold drop-shadow-lg text-4xl '>Welcome to Your Password Generator</h1>
+      <div className=' flex justify-center items-center mt-5 bg-amber-400 hover:bg-amber-500 drop-shadow-xl box-shadow-lg border-amber-900 border-2   rounded-lg py-4 px-10'>
 
       <div className=''>
-        <div className='w-64 border-blue-600 border-2 flex justify-center py-1 mx-auto rounded-md bg-white  '>
-          <input type="text" readOnly className='rounded-lg border-none focus:outline-none' placeholder='password' value={password}/>
-          <button onClick={copyToClip} className='  text-center bg-blue-400 rounded-xl  px-2 py-1'>Copy</button>
+        <div className='w-fit border-blue-600 border-2 flex justify-center py-1 px-2 mx-auto rounded-md bg-white  '>
+          <input type="text" 
+          readOnly className='rounded-lg border-none focus:outline-none' 
+          placeholder='password' 
+          value={password}
+          ref={passRef}
+          />
+          <button onClick={copyToClip} className='  text-center bg-blue-400 hover:bg-blue-600 hover:text-amber-300 mx-2 transition-all ease-in-out duration-300 active:bg-blue-400 rounded-xl  px-2 py-1'>Copy</button>
+          <button className='bg-gray-300 font-outfit hover:bg-gray-400 active:bg-gray-300 hover:text-amber-300 transition-all ease-in-out duration-300 rounded-lg py-1 px-5'
+          onClick={()=> {passwordGenerator()}}>Change Password</button>
         </div>
 
         <div className='flex justify-center items-center py-5'>
-          <div className='flex cursor-pointer mx-2'>
+          <div className='flex cursor-pointer mx-2 font-outfit'>
           <input type="range" 
           min={6}
           max={12}
           value={length}
           id="lengthInput"
-          ref={passRef}
           onChange={(e) => {setLength(parseInt(e.target.value))}}
           />
           <label htmlFor='lengthInput' className='flex justify-center items-center mx-1 px-5'>Length({length})</label>  
-          <button className='bg-gray-300 hover:bg-gray-400 hover:text-amber-300 rounded-lg py-1 px-5'
-          onClick={()=> {passwordGenerator()}}>Change Password</button>
+      
           </div>
 
           <div className='flex mx-2 '>
@@ -71,7 +76,7 @@ useEffect(()=>{
             className='mx-1'
             onChange={()=>{setAllowNum((prev)=> !prev)}}
             id="numInput" />
-            <label htmlFor='numInput'>Number</label>
+            <label className='font-outfit' htmlFor='numInput'>Number</label>
           </div>
 
           <div className='flex '>
@@ -80,7 +85,7 @@ useEffect(()=>{
             type="checkbox"
             id= "charInput"
             className='mx-1'/>
-            <label htmlFor='charInput'>Character</label>
+            <label className='font-outfit' htmlFor='charInput'>Character</label>
           </div>
         </div>
 
